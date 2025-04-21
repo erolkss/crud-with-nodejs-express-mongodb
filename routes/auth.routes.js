a16d9249994ee1e7c00c1644030fcd5342f83a15
Lucas Lima <lucaserolima@gmail.com>
2025-04-21 17:22:41 -0400

    feat(auth): implement user signup with hashed password and JWT

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,10 @@
 import {Router} from 'express';
+import {signIn, signOut, signUp} from "../controllers/auth.controller.js";
 
 const authRoutes = Router();
 
-authRoutes.post('/sign-up', (req, res) =>
-    res.send({title: 'Sign up'})
-);
-authRoutes.post('/sign-in', (req, res) =>
-    res.send({title: 'Sign in'})
-);
-authRoutes.post('/sign-out', (req, res) =>
-    res.send({title: 'Sign out'})
-);
+authRoutes.post('/sign-up', signUp);
+authRoutes.post('/sign-in', signIn);
+authRoutes.post('/sign-out', signOut);
 
 export default authRoutes;
