1b01323b5f735f049e831e29125f66191c6a38d1
Lucas Lima <lucaserolima@gmail.com>
2025-04-21 22:13:06 -0400

    feat(auth): add JWT authorization middleware and protect getUser route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,11 @@
 import {Router} from 'express';
+import {getUser, getUsers} from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRoutes = Router();
 
-userRoutes.get('/', (req, res) =>
-    res.send({title: 'GET all users'})
-);
-userRoutes.get('/:id', (req, res) =>
-    res.send({title: 'GET user details'})
-);
+userRoutes.get('/', getUsers);
+userRoutes.get('/:id', authorize, getUser);
 userRoutes.post('/', (req, res) =>
     res.send({title: 'Create new user'})
 );
